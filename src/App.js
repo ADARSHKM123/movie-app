078fbf183e47c0a9c2c729716d05a5779cb0533b
ADARSH123 <adarshkmeethal@gmail.com>
2023-01-25 22:10:38 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,62 +1,167 @@
 import './App.css';
 import { useState } from 'react';
 
-const movieDta = [
-  {
-    name:"Sholay",
-    date:"23-21-2022",
-    time: "5:00 PM"
-  },
-  {
-    name: "The Shawshank Redemption",
-    date: "2022-05-01",
-    time: "10:00 AM"
-  },
-  {
-    name: "The Godfather",
-    date: "2022-05-02",
-    time: "2:00 PM"
-  },
-  {
-    name: "The Dark Knight",
-    date: "2022-05-03",
-    time: "9:00 PM"
-  }
-]
 
 
 function App() {
 
   const [showing, setshow] = useState(false);
+  const [showingywo, setshowtwo] = useState(false);
+  const [showingthree, setshowthree] = useState(false);
 
-  const showSeats =(e)=>{
+  const showSeats = (e) => {
     console.log('clicked');
+    console.log(e.target.value);
+    const mainVal = e.target.value
+    if (mainVal === '1') {
+      setshow(!showing);
+    } else if (mainVal === '2') {
+      setshowtwo(!showingywo);
+    } else if (mainVal === '3') {
+      setshowthree(!showingthree)
+    }
     console.log(showing);
-    setshow(!showing);
   }
 
-  const selectSeat =(e)=>{
-   console.log('selected');
+  const selectSeat = (e) => {
+    e.preventDefault();
+    console.log(e.target.data);
+    console.log('selected');
   }
 
   return (
     <div className="App">
-          <div className='movies'>
-          <div>
-            <p>manikkam</p>
+      <div className='movies'>
+        <div>
+          <p>manikkam</p>
+        </div>
+        <div>
+          <p>23-20-2022</p>
+        </div>
+        <div>
+          <p>3:00 PM</p>
+        </div>
+        <div>
+          <button className='btn' onClick={showSeats} value={1}>Available Seats</button>
+        </div>
+      </div>
+      {showing ? <div className='seats'>
+        <div className='container'>
+          <div className='row'>
+            <div className='seat' data="My div value"></div>
+            <div className='seat booked' onClick={selectSeat} value="one"></div>
+            <div className='seat booked'  ></div>
+            <div className='seat '></div>
+
+            <div className='seat'></div>
+            <div className='seat booked'></div>
+            <div className='seat'></div>
+            <div className='seat'></div>
+            <div className='seat'></div>
+            <div className='seat'></div>
+            <div className='seat'></div>
+
+            <div className='seat '></div>
+            <div className='seat'></div>
+            <div className='seat booked'></div>
+            <div className='seat'></div>
+
           </div>
-          <div>
-            <p>23-20-2022</p>
+          <div className='row'>
+            <div className='seat booked'></div>
+            <div className='seat'></div>
+            <div className='seat'></div>
+            <div className='seat booked '></div>
+            <div className='seat'></div>
+            <div className='seat'></div>
+            <div className='seat'></div>
+            <div className='seat'></div>
+            <div className='seat'></div>
+            <div className='seat'></div>
+            <div className='seat'></div>
+
+            <div className='seat '></div>
+            <div className='seat'></div>
+            <div className='seat booked'></div>
+            <div className='seat'></div>
           </div>
-          <div>
-            <p>3:00 PM</p>
+          <div className='row'>
+            <div className='seat'></div>
+            <div className='seat'></div>
+            <div className='seat'></div>
+            <div className='seat booked '></div>
+            <div className='seat'></div>
+            <div className='seat booked'></div>
+            <div className='seat booked'></div>
+            <div className='seat'></div>
+            <div className='seat'></div>
+            <div className='seat'></div>
+            <div className='seat'></div>
+
+            <div className='seat '></div>
+            <div className='seat'></div>
+            <div className='seat'></div>
+            <div className='seat'></div>
           </div>
-          <div>
-            <button className='btn' onClick={showSeats}>Available Seats</button>
+          <div className='row frontseat'>
+            <div className='seat'></div>
+            <div className='seat'></div>
+            <div className='seat'></div>
+            <div className='seat '></div>
+            <div className='seat'></div>
+            <div className='seat'></div>
+            <div className='seat'></div>
+            <div className='seat'></div>
+            <div className='seat'></div>
+            <div className='seat'></div>
+            <div className='seat'></div>
+
+            <div className='seat '></div>
+            <div className='seat'></div>
+            <div className='seat '></div>
+            <div className='seat'></div>
+          </div>
+          <div className='row'>
+            <div className='seat'></div>
+            <div className='seat'></div>
+            <div className='seat'></div>
+            <div className='seat '></div>
+            <div className='seat'></div>
+            <div className='seat'></div>
+            <div className='seat'></div>
+            <div className='seat'></div>
+            <div className='seat'></div>
+            <div className='seat'></div>
+            <div className='seat'></div>
+
+
+            <div className='seat '></div>
+            <div className='seat'></div>
+            <div className='seat booked'></div>
+            <div className='seat'></div>
           </div>
         </div>
-        {showing ? <div className='seats'>
-          <div className='container'>
+        <button className='btt'>Book Now</button>
+      </div> : ''}
+
+
+      <div className='movies'>
+        <div>
+          <p>manikkam</p>
+        </div>
+        <div>
+          <p>23-20-2022</p>
+        </div>
+        <div>
+          <p>3:00 PM</p>
+        </div>
+        <div>
+          <button className='btn' onClick={showSeats} value={2}>Available Seats</button>
+        </div>
+      </div>
+
+      {showingywo ? <div className='seats'>
+        <div className='container'>
           <div className='row'>
             <div className='seat'></div>
             <div className='seat booked' onClick={selectSeat}></div>
@@ -76,8 +181,8 @@ function App() {
             <div className='seat booked'></div>
             <div className='seat'></div>
 
-            </div>
-            <div className='row'>
+          </div>
+          <div className='row'>
             <div className='seat booked'></div>
             <div className='seat'></div>
             <div className='seat'></div>
@@ -94,8 +199,8 @@ function App() {
             <div className='seat'></div>
             <div className='seat booked'></div>
             <div className='seat'></div>
-            </div>
-            <div className='row'>
+          </div>
+          <div className='row'>
             <div className='seat'></div>
             <div className='seat'></div>
             <div className='seat'></div>
@@ -112,8 +217,8 @@ function App() {
             <div className='seat'></div>
             <div className='seat'></div>
             <div className='seat'></div>
-            </div>
-            <div className='row frontseat'>
+          </div>
+          <div className='row frontseat'>
             <div className='seat'></div>
             <div className='seat'></div>
             <div className='seat'></div>
@@ -130,8 +235,8 @@ function App() {
             <div className='seat'></div>
             <div className='seat '></div>
             <div className='seat'></div>
-            </div>
-            <div className='row'>
+          </div>
+          <div className='row'>
             <div className='seat'></div>
             <div className='seat'></div>
             <div className='seat'></div>
@@ -149,42 +254,124 @@ function App() {
             <div className='seat'></div>
             <div className='seat booked'></div>
             <div className='seat'></div>
-            </div>
-            </div>
-            
-          </div>: ''}
-
-
-        <div className='movies'>
-          <div>
-            <p>manikkam</p>
-          </div>
-          <div>
-            <p>23-20-2022</p>
-          </div>
-          <div>
-            <p>3:00 PM</p>
-          </div>
-          <div>
-            <button>Available Seats</button>
           </div>
         </div>
+        <button className='btt'>Book Now</button>
+      </div> : ''}
 
-        <div className='movies'>
-          <div>
-            <p>manikkam</p>
+      <div className='movies'>
+        <div>
+          <p>manikkam</p>
+        </div>
+        <div>
+          <p>23-20-2022</p>
+        </div>
+        <div>
+          <p>3:00 PM</p>
+        </div>
+        <div>
+          <button className='btn' onClick={showSeats} value={3}>Available Seats</button>
+        </div>
+      </div>
+
+      {showingthree ? <div className='seats'>
+        <div className='container'>
+          <div className='row'>
+            <div className='seat' onClick={selectSeat}></div>
+            <div className='seat booked' onClick={selectSeat}></div>
+            <div className='seat booked'></div>
+            <div className='seat '></div>
+
+            <div className='seat'></div>
+            <div className='seat booked'></div>
+            <div className='seat'></div>
+            <div className='seat'></div>
+            <div className='seat'></div>
+            <div className='seat'></div>
+            <div className='seat'></div>
+
+            <div className='seat '></div>
+            <div className='seat'></div>
+            <div className='seat booked'></div>
+            <div className='seat'></div>
+
           </div>
-          <div>
-            <p>23-20-2022</p>
+          <div className='row'>
+            <div className='seat booked'></div>
+            <div className='seat'></div>
+            <div className='seat'></div>
+            <div className='seat booked '></div>
+            <div className='seat'></div>
+            <div className='seat'></div>
+            <div className='seat'></div>
+            <div className='seat'></div>
+            <div className='seat'></div>
+            <div className='seat'></div>
+            <div className='seat'></div>
+
+            <div className='seat '></div>
+            <div className='seat'></div>
+            <div className='seat booked'></div>
+            <div className='seat'></div>
           </div>
-          <div>
-            <p>3:00 PM</p>
+          <div className='row'>
+            <div className='seat'></div>
+            <div className='seat'></div>
+            <div className='seat'></div>
+            <div className='seat booked '></div>
+            <div className='seat'></div>
+            <div className='seat booked'></div>
+            <div className='seat booked'></div>
+            <div className='seat'></div>
+            <div className='seat'></div>
+            <div className='seat'></div>
+            <div className='seat'></div>
+
+            <div className='seat '></div>
+            <div className='seat'></div>
+            <div className='seat'></div>
+            <div className='seat'></div>
           </div>
-          <div>
-            <button>Book Now</button>
+          <div className='row frontseat'>
+            <div className='seat'></div>
+            <div className='seat'></div>
+            <div className='seat'></div>
+            <div className='seat '></div>
+            <div className='seat'></div>
+            <div className='seat'></div>
+            <div className='seat'></div>
+            <div className='seat'></div>
+            <div className='seat'></div>
+            <div className='seat'></div>
+            <div className='seat'></div>
+
+            <div className='seat '></div>
+            <div className='seat'></div>
+            <div className='seat '></div>
+            <div className='seat'></div>
+          </div>
+          <div className='row'>
+            <div className='seat'></div>
+            <div className='seat'></div>
+            <div className='seat'></div>
+            <div className='seat '></div>
+            <div className='seat'></div>
+            <div className='seat'></div>
+            <div className='seat'></div>
+            <div className='seat'></div>
+            <div className='seat'></div>
+            <div className='seat'></div>
+            <div className='seat'></div>
+
+
+            <div className='seat'></div>
+            <div className='seat'></div>
+            <div className='seat booked'></div>
+            <div className='seat'></div>
           </div>
         </div>
-        
+        <button className='btt'>Book Now</button>
+      </div> : ''}
     </div>
   );
 }
